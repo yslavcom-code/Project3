85da1d330bd4adeb60b7f15368c5ec4cdb669a01
Soheil Dabooyeh <soheil.dabooyeh29@gmail.com>
2024-12-08 16:45:29 +1100

    Update-Add more visualisation

--- BankProject/app.py
+++ BankProject/app.py
@@ -21,6 +21,33 @@ def get_countries():
                 if key not in ['_id', 'year', 'indicator_type', 
                              'file_source', 'seasonally_adjusted']]
     return jsonify({"countries": sorted(countries)})
-
+@app.route('/api/economic-data')
+def get_economic_data():
+    # Get the parameters sent by the user's selections
+    countries = request.args.getlist('countries[]')  
+    indicator = request.args.get('indicator', 'GDP')  
+    
+    # Connect to our database
+    db = connect_to_database()
+    collection = db["cleaned_economic_data"]
+    
+    # Set up our database query
+    query = {"indicator_type": indicator}
+    
+    # Specify which fields we want to get from the database
+    projection = {
+        "year": 1,    # We always need the year
+        "_id": 0      # We don't need MongoDB's internal ID
+    }
+    
+    # Add each selected country to our projection
+    for country in countries:
+        projection[country] = 1
+        
+    # Get the data and sort it by year
+    data = list(collection.find(query, projection).sort("year", 1))
+    
+    # Send the data back to the webpage
+    return jsonify({"data": data})
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
